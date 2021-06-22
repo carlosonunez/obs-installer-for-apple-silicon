382d4f40a224b392761c2617196218913371c5c2
Carlos Nunez <carlosonunez@noreply.github.com>
2021-06-22 16:52:58 -0500

    Rest of the owl

--- install.sh
+++ install.sh
@@ -1,10 +1,32 @@
 #!/usr/bin/env bash
 LOG_LEVEL="${LOG_LEVEL:-info}"
 OBS_INSTALL_DIR="/tmp/obs"
+OBS_DEPS_DIR="/tmp/obsdeps"
 OBS_GIT_URI=https://github.com/obsproject/obs-studio.git
+OBS_DEPS_GIT_URI=https://github.com/obsproject/obs-deps.git
+OBS_DMG_PATH=obs-studio-x64-27.0.1-2-g3cc4feb8d-modified.dmg
+FINAL_OBS_DMG_PATH="$HOME/Downloads/$OBS_DMG_PATH"
+SPEEX_DIR=/tmp/speexdsp
+SPEEX_URI=https://github.com/xiph/speexdsp.git
 
 _log() {
   echo "[$(date)] $(echo "$1" | tr '[:lower:]' '[:upper:]'): $2"
+}
+
+_fetch() {
+  name="$1"
+  dest="$2"
+  git_uri="$3"
+  if ! test -d "$dest"
+  then
+    log_info "Downloading [$name] from [$git_uri] to $dest. This might take a while; please be patient."
+    if ! git clone --recursive "$git_uri" "$dest"
+    then
+      fail "Unable to download [$name]."
+    fi
+  else
+    log_debug "[$name] repo already downloaded; skipping."
+  fi
 }
 
 log_debug() {
@@ -50,17 +72,17 @@ install_dependencies_or_fail() {
 }
 
 download_obs_or_fail() {
-  if ! test -d "$OBS_INSTALL_DIR"
-  then
-    log_info "Downloading OBS to $OBS_INSTALL_DIR. This might take a while; please be patient."
-    if ! git clone --recursive "$OBS_GIT_URI" "$OBS_INSTALL_DIR"
-    then
-      fail "Unable to download OBS."
-    fi
-  else
-    log_debug "OBS repo already downloaded; skipping."
-  fi
+  _fetch "OBS" "$OBS_INSTALL_DIR" "$OBS_GIT_URI"
 }
+
+download_obs_deps_or_fail() {
+  _fetch "OBS Dependencies" "$OBS_DEPS_DIR" "$OBS_DEPS_GIT_URI"
+}
+
+fetch_speexdsp_source() {
+  _fetch "SpeexDSP" "$SPEEX_DIR" "$SPEEX_URI"
+}
+
 
 copy_modified_files_into_cloned_repo() {
   while read -r file
@@ -91,17 +113,73 @@ copy_templates_into_cloned_repo() {
   done < <(find template -type f | grep -Ev '(Instructions|DS_Store)')
 }
 
-build_obs() {
-  pushd "$OBS_INSTALL_DIR"
-  if !  cmake -DCMAKE_OSX_DEPLOYMENT_TARGET=10.13 \
-      -DDISABLE_PYTHON=ON \
-      ..//waitmake//waitcd \
-      rundir/RelWithDebInfo/bin
+build_obs_or_fail() {
+  pushd "$OBS_INSTALL_DIR/cmake"
+  if ! (
+    cmake -DCMAKE_OSX_DEPLOYMENT_TARGET=10.13 -DDISABLE_PYTHON=ON \
+      -DCMAKE_PREFIX_PATH="/opt/homebrew/opt/qt@5" \
+      -DSPEEXDSP_INCLUDE_DIR="$SPEEX_DIR/include" \
+      -DSWIGDIR="$OBS_DEPS_DIR" \
+      -DDepsPath="$OBS_DEPS_DIR" .. &&
+    make &&
+    stat rundir/RelWithDebInfo/bin/obs 1>/dev/null
+  )
   then
     popd &>/dev/null
     fail "Unable to build OBS; see above logs for more info."
   fi
   popd &>/dev/null
+}
+
+package_obs_or_fail() {
+  if ! test -f "$OBS_INSTALL_DIR/cmake/$OBS_DMG_PATH"
+  then
+    log_info "Packaging OBS"
+    pushd "$OBS_INSTALL_DIR/cmake"
+    if ! ( cpack && test -f "$OBS_INSTALL_DIR/cmake/$OBS_DMG_PATH" )
+    then
+      popd &>/dev/null
+      fail "Unable to package OBS; see above logs for more info."
+    fi
+    popd &>/dev/null
+  fi
+}
+
+add_virtualcam_plugin() {
+  log_info "Adding MacOS Virtual Camera plugin."
+  if test -f "$OBS_INSTALL_DIR/obs.dmg"
+  then
+    rm "$OBS_INSTALL_DIR/obs.dmg"
+  fi
+  if ! (
+    hdiutil convert -format UDRW -o "$OBS_INSTALL_DIR/obs.dmg" \
+      "$OBS_INSTALL_DIR/cmake/$OBS_DMG_PATH" &&
+    hdiutil attach "$OBS_INSTALL_DIR/obs.dmg"
+  )
+  then
+    fail "Unable to create or attach to writeable OBS image; see logs for more."
+  fi
+  device=$(hdiutil attach "$OBS_INSTALL_DIR/obs.dmg" | \
+    tail -1 | \
+    awk '{print $1}')
+  mountpath=$(hdiutil attach "$OBS_INSTALL_DIR/obs.dmg" | \
+    tail -1 | \
+    awk '{print $3}')
+  cp -r "$OBS_INSTALL_DIR/cmake/rundir/RelWithDebInfo/data/obs-mac-virtualcam.plugin" \
+    "$mountpath/OBS.app/Contents/Resources/data"
+  hdiutil detach "$device"
+}
+
+repackage_obs_or_fail() {
+  log_info "Re-packaging OBS with Virtual Camera added."
+  hdiutil convert -format UDRO -o "$FINAL_OBS_DMG_PATH" "$OBS_INSTALL_DIR/obs.dmg"
+}
+
+remove_data_directories() {
+  log_info "Cleaning up"
+  rm -rf "$OBS_INSTALL_DIR" &&
+    rm -rf "$OBS_DEPS_DIR" &&
+    rm -rf "$SPEEX_DIR"
 }
 
 if ! homebrew_installed
@@ -111,6 +189,16 @@ fi
 
 install_dependencies_or_fail
 download_obs_or_fail
+download_obs_deps_or_fail
 copy_modified_files_into_cloned_repo
 copy_templates_into_cloned_repo
-build_obs
+fetch_speexdsp_source
+build_obs_or_fail
+package_obs_or_fail
+add_virtualcam_plugin
+repackage_obs_or_fail
+#remove_data_directories
+
+log_info "Installation succeeded! Move OBS into your Applications folder in the \
+Finder window that pops up."
+open "$FINAL_OBS_DMG_PATH"
